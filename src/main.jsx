3ff6c6a08607c2326d8ddd55d31b6edc6fc6fa34
tayosdesign <tayosdesign@gmail.com>
2026-04-13 15:52:28 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,4 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './LOxalate3.jsx'
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode><App /></React.StrictMode>)
